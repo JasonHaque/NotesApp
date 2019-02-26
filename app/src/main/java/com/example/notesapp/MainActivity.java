b11069c700e91eead0ff8104ef28a0f67af05187
jason <zahinjason220434@gmail.com>
2019-02-26 22:37:21 +0600

    FireBase Support Added & Sign Up layout Added

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.notesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button SignUp,LogIn;
+    private EditText mailText,passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bindlisteners();
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }
+        });
+
+
+    }
+
+    public void Bindlisteners(){
+        //firebaseAuth = FirebaseAuth.getInstance();
+        SignUp=findViewById(R.id.sign_up_button);
+        LogIn=findViewById(R.id.log_in_button);
+        mailText=findViewById(R.id.log_in_mail);
+        passwordText= findViewById(R.id.log_in_password);
     }
 }
