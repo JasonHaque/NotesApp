7265395ac2365bf15bc0731f03a0c528b655df8b
jason <zahinjason220434@gmail.com>
2019-04-21 00:40:19 +0600

    Added profile

--- app/src/main/java/com/example/notesapp/profile.java
+++ app/src/main/java/com/example/notesapp/profile.java
@@ -2,12 +2,26 @@ package com.example.notesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class profile extends AppCompatActivity {
 
+
+    private TextView profileText;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        profileText = findViewById(R.id.profile_view);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        String[] user=firebaseAuth.getCurrentUser().getEmail().split("@");
+        String userName=user[0];
+
+        profileText.setText("Welcome "+userName);
+
     }
 }
