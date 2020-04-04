31ee7fc3c35434dd4b0d2a4de1214de7accd03c5
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 03:54:30 +0600

    activity added

---
+++ Code/app/src/main/java/com/example/noteapp/authentication/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.noteapp.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.noteapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
