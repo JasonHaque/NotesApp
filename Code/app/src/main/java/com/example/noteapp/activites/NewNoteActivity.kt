fd112682e037b309723094c87ab7130404bacf9f
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 05:42:09 +0600

    layout change

--- Code/app/src/main/java/com/example/noteapp/activites/NewNoteActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/activites/NewNoteActivity.kt
@@ -3,11 +3,18 @@ package com.example.noteapp.activites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.R
+import kotlinx.android.synthetic.main.activity_new_note.*
 
 class NewNoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_note)
+        bindListeners()
+    }
+    private fun bindListeners(){
+        save_note.setOnClickListener {
+            //TODO: implement saving functionality
+        }
     }
 }
