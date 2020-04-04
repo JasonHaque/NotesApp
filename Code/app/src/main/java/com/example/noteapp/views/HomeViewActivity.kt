e269ced3d840be616ffe74b5f334e5b969aeb2e7
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 03:56:25 +0600

    activity added

---
+++ Code/app/src/main/java/com/example/noteapp/views/HomeViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.noteapp.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_view)
+    }
+}
