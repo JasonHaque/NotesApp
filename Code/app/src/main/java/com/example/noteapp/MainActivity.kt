1863ce04008ab3fd8ef0b6867c8bd62d80e1d1cf
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 04:03:44 +0600

    layout added

--- Code/app/src/main/java/com/example/noteapp/MainActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.noteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.noteapp.authentication.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
     }
 }
