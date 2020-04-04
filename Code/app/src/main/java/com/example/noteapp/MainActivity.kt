d7a05cdbab85b65603139e09a40375498a05759e
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 05:34:07 +0600

    added user status check

--- Code/app/src/main/java/com/example/noteapp/MainActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.authentication.LoginActivity
+import com.example.noteapp.views.HomeViewActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this,LoginActivity::class.java)
+        val intent = checkUserStatus()
         startActivity(intent)
+
+    }
+
+    private fun checkUserStatus() : Intent{
+        if (FirebaseAuth.getInstance().currentUser != null){
+            val intent = Intent(this,HomeViewActivity::class.java)
+            return intent
+        }
+        val intent = Intent(this,LoginActivity::class.java)
+        return intent
     }
 }
