dba5c0da37735a0f8af68e55d269a4be50652b2b
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 05:02:15 +0600

    added button

--- Code/app/src/main/java/com/example/noteapp/views/HomeViewActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/views/HomeViewActivity.kt
@@ -3,11 +3,19 @@ package com.example.noteapp.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.R
+import kotlinx.android.synthetic.main.activity_home_view.*
 
 class HomeViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view)
+        bindListeners()
+    }
+
+    private fun bindListeners(){
+        new_note_page.setOnClickListener {
+
+        }
     }
 }
