2aadd9f18fee8d07681af182247fa71564777154
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 05:25:00 +0600

    log out method added

--- Code/app/src/main/java/com/example/noteapp/views/ProfileView.kt
+++ Code/app/src/main/java/com/example/noteapp/views/ProfileView.kt
@@ -1,13 +1,25 @@
 package com.example.noteapp.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.R
+import com.example.noteapp.authentication.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_profile_view.*
 
 class ProfileView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_view)
+        bindListeners()
+    }
+    private fun bindListeners(){
+        log_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
