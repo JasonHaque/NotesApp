8986c006783ab1d4ad0dce5ec71c9d460e5205d3
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 04:21:54 +0600

    added routes

--- Code/app/src/main/java/com/example/noteapp/authentication/LoginActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/authentication/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.example.noteapp.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        bindListeners()
+    }
+
+    private fun bindListeners(){
+        go_to_sign_up.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Code/app/src/main/java/com/example/noteapp/authentication/SignUpActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/authentication/SignUpActivity.kt
@@ -1,13 +1,23 @@
 package com.example.noteapp.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        bindListeners()
+    }
+    private fun bindListeners(){
+        go_to_log_in.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
