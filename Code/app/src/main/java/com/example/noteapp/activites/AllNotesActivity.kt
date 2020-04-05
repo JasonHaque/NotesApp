7543f255584e283078aff582a0920d6c6a58e7be
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 20:55:53 +0600

    git added

--- Code/app/src/main/java/com/example/noteapp/activites/AllNotesActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/activites/AllNotesActivity.kt
@@ -1,28 +1,73 @@
 package com.example.noteapp.activites
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapp.R
 import com.example.noteapp.adapters.NoteAdapter
 import com.example.noteapp.classes.Notes
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class AllNotesActivity : AppCompatActivity() {
-
+    private var db = FirebaseDatabase.getInstance().reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_notes)
         val recyclerView = findViewById<RecyclerView>(R.id.note_recycler_view)
         recyclerView.layoutManager=LinearLayoutManager(this,RecyclerView.VERTICAL,false)
         val notes = ArrayList<Notes>()
-        notes.add(Notes("Hi","Here I am"))
-        notes.add(Notes("Hi","Here I am"))
-        notes.add(Notes("Hi","Here I am"))
-        notes.add(Notes("Hi","Here I am"))
-        notes.add(Notes("Hi","Here I am"))
+        val usermail = FirebaseAuth.getInstance().currentUser?.email.toString()
+        val split =usermail.split("@")
+        val user = split[0]
+        print("Hello from here")
+        db.child("Users").child(user).addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                println("Error receiving data")
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+
+                if (p0.exists()){
+                    val dataMap = p0.value as HashMap<String,Any>?
+                    print(dataMap)
+                    for (key in dataMap!!.keys){
+                        val data = dataMap!![key]
+                        val userdata =
+                            data as HashMap<String, Any>?
+                        print(userdata)
+                        val abs = Notes(
+                            userdata!!["noteName"] as String,
+                            userdata["noteContent"] as String
+                        )
+                        notes.add(abs)
+
+
+                    }
+                }
+
+
+
+            }
+
+        })
+
+
+
+
+
+
+
+
+
+
         val adapter = NoteAdapter(notes)
         recyclerView.adapter = adapter
     }
+
 }
