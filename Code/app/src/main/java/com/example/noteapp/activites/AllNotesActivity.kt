a3a1614d6899e13c0bba8f4ca8f1eb466e6101c7
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 19:10:12 +0600

    adpater created

--- Code/app/src/main/java/com/example/noteapp/activites/AllNotesActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/activites/AllNotesActivity.kt
@@ -2,12 +2,27 @@ package com.example.noteapp.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapp.R
+import com.example.noteapp.adapters.NoteAdapter
+import com.example.noteapp.classes.Notes
 
 class AllNotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_notes)
+        val recyclerView = findViewById<RecyclerView>(R.id.note_recycler_view)
+        recyclerView.layoutManager=LinearLayoutManager(this,RecyclerView.VERTICAL,false)
+        val notes = ArrayList<Notes>()
+        notes.add(Notes("Hi","Here I am"))
+        notes.add(Notes("Hi","Here I am"))
+        notes.add(Notes("Hi","Here I am"))
+        notes.add(Notes("Hi","Here I am"))
+        notes.add(Notes("Hi","Here I am"))
+        val adapter = NoteAdapter(notes)
+        recyclerView.adapter = adapter
     }
 }
