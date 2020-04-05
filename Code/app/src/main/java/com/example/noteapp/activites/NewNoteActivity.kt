0f1f290d958c49d65a3dd5a9232293e3bb328594
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 12:47:26 +0600

    added input verification

--- Code/app/src/main/java/com/example/noteapp/activites/NewNoteActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/activites/NewNoteActivity.kt
@@ -2,6 +2,7 @@ package com.example.noteapp.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.noteapp.R
 import kotlinx.android.synthetic.main.activity_new_note.*
 
@@ -14,7 +15,22 @@ class NewNoteActivity : AppCompatActivity() {
     }
     private fun bindListeners(){
         save_note.setOnClickListener {
-            //TODO: implement saving functionality
+            val noteName = note_name.text.toString()
+            val noteContent = note_description.text.toString()
+            val error = verifyInputs(noteName, noteContent)
+            if(!error.equals("")){
+                Toast.makeText(this,error,Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
         }
+    }
+
+    private fun verifyInputs(noteName:String,noteContent:String):String{
+        if (noteName.isEmpty() || noteContent.isEmpty()){
+            return "Fill up the fields properly"
+        }
+
+
+        return ""
     }
 }
