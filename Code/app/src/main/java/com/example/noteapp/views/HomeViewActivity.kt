7c4f44f4ced54fcdcd0d21e4075e78009f428455
JasonHaque <zahinjason220434@gmail.com>
2020-04-05 19:12:27 +0600

    route created

--- Code/app/src/main/java/com/example/noteapp/views/HomeViewActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/views/HomeViewActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.noteapp.R
+import com.example.noteapp.activites.AllNotesActivity
 import com.example.noteapp.activites.NewNoteActivity
 import kotlinx.android.synthetic.main.activity_home_view.*
 
@@ -18,6 +19,10 @@ class HomeViewActivity : AppCompatActivity() {
     private fun bindListeners(){
         new_note_page.setOnClickListener {
             val intent =Intent(this,NewNoteActivity::class.java)
+            startActivity(intent)
+        }
+        all_notes_page.setOnClickListener {
+            val intent =Intent(this,AllNotesActivity::class.java)
             startActivity(intent)
         }
         profile_page.setOnClickListener {
