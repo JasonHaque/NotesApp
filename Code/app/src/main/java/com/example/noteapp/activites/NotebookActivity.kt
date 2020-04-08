8afe45289bb4244066821553a151ed3cf8c9a716
JasonHaque <zahinjason220434@gmail.com>
2020-04-09 05:40:19 +0600

    added new view code

--- Code/app/src/main/java/com/example/noteapp/activites/NotebookActivity.kt
+++ Code/app/src/main/java/com/example/noteapp/activites/NotebookActivity.kt
@@ -2,12 +2,23 @@ package com.example.noteapp.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.noteapp.R
+import kotlinx.android.synthetic.main.activity_notebook.*
 
 class NotebookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notebook)
+        bindViews()
+        bindListeners()
+    }
+
+    fun bindViews(){
+        note_book_name.visibility = View.GONE
+    }
+    fun bindListeners(){
+
     }
 }
